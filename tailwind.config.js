f36da7bc2cce40b55e5e2ddcdbd34ad390a1e315
Lucien Bénié <lucien.benie@logmein.com>
2020-09-20 16:43:15 -0400

    purge css

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,4 +3,12 @@ module.exports = {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
   },
+  purge: {
+    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
+    enabled: process.env.NODE_ENV === 'production',
+    content: ['src/**/*.vue'],
+    options: {
+      whitelistPatternsChildren: [/^token/, /^pre/, /^code/],
+    },
+  },
 }
